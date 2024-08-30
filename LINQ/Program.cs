37f0770bfbe4d89bf3df84d48d704948505fb5bc
Mariah McNicol <mariahmcnicol@gmail.com>
2024-08-30 16:06:12 -0400

    completed LINQ exercise 1 with video game names

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace LINQ
+﻿using Console = System.Console;
+    
+using System.Linq;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //Instructions:
+        // x Create a list of video game names...
+        // Order the list of games by length of the game name.
+        // Use the lambda expression in this exercise as well.
+        // Use Method Syntax for this exercise
+
+        //Here is my list of video game names: 
+        var videoGames = new List<string>()
         {
-            Console.WriteLine("Hello, World!");
+            "Fallout", "Red Dead Redemption", "Powerwashing Simulator", "Fortnite", "Baldur's Gate 3", "Unpacking",
+            "Halo", "God of War", "The Sims", "Untitled Goose Game"
+        };
+        
+        //Sorting the list:
+        var sortedVideoGames = videoGames.OrderBy(game => game.Length).ToList();
+        
+        //Displaying the results: 
+        Console.WriteLine("Here is the list of games sorted in ascending order by length:");
+        foreach (var game in sortedVideoGames)
+        {
+            Console.WriteLine(game);
         }
-    }
 }
+}
+
